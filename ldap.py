42e95d4560787492ef0a5f6d90b6ff7dcb5a6e2c
szetochunkit <junjietusi@163.com>
2024-07-23 11:54:09 +0800

    Update ldap.py

--- ldap.py
+++ ldap.py
@@ -1,6 +1,7 @@
 import ssl
 from ldap3 import Tls, NTLM, Connection, Server, SUBTREE, MODIFY_REPLACE
-
+from ldap3.extend.microsoft.addMembersToGroups import ad_add_members_to_groups
+from ldap3.extend.microsoft.removeMembersFromGroups import ad_remove_members_from_groups
 from utils import decode_str, get_config_value
 
 config_path = 'config.ini'
@@ -50,14 +51,14 @@ class LDAP_OP(object):
         try:
             if not conn.bind():
                 print("Not Connected")
-                conn.unbind()
+                #conn.unbind()
                 return False
             else:
                 print("Connected")
-                conn.unbind()
+                #conn.unbind()
                 return True
         finally:
-            pass
+            conn.unbind()
 
     def search_user(self, conn, username):
         if conn == False:
@@ -67,8 +68,11 @@ class LDAP_OP(object):
                                                           '(samaccountname=' + username + ')' \
                                                                                           '(mail=' + username + '))' \
                                                                                                                 '(objectClass=person))'
-        conn.search(search_base=base_dn, search_filter=SEARCHFILTER,
-                    attributes=['*'], search_scope=SUBTREE, paged_size=5)
+        conn.search(search_base=base_dn,
+                    search_filter=SEARCHFILTER,
+                    attributes=['*'],
+                    search_scope=SUBTREE,
+                    paged_size=5)
                     #search_scope=SUBTREE, attributes=['cn', 'mail'], paged_size=5)
 
         for entry in conn.response:
@@ -93,3 +97,80 @@ class LDAP_OP(object):
         except Exception as e:
             return False
 
+    def modify_user(self, conn, username, attribute, new_value):
+        if conn == False:
+            return "bind failed"
+        search_user_res = self.search_user(conn, username)
+        if search_user_res == "user not exist":
+            return search_user_res
+        user_dn = search_user_res["distinguishedName"]
+        modify_res = conn.modify(user_dn, {attribute: [(MODIFY_REPLACE, [new_value])]})
+        return modify_res
+
+    def disable_user(self, conn, username):
+        if conn == False:
+            return "bind failed"
+        search_user_res = self.search_user(conn, username)
+        if search_user_res == "user not exist":
+            return search_user_res
+        user_dn = search_user_res["distinguishedName"]
+        modify_res = conn.modify(user_dn, {"userAccountControl": [(MODIFY_REPLACE, ["514"])]})
+        return modify_res
+
+    def enable_user(self, conn, username):
+        if conn == False:
+            return "bind failed"
+        search_user_res = self.search_user(conn, username)
+        if search_user_res == "user not exist":
+            return search_user_res
+        user_dn = search_user_res["distinguishedName"]
+        modify_res = conn.modify(user_dn, {"userAccountControl": [(MODIFY_REPLACE, ["512"])]})
+        return modify_res
+
+    def search_group(self, conn, group_name):
+        #SEARCHFILTER = f'(&(objectClass=group)(samaccountname={group_name}))'
+
+        SEARCHFILTER = "(&(objectClass=group)(samaccountname=%s))" % (group_name)
+        conn.extend.standard.paged_search(
+            search_base=base_dn,
+            search_filter=SEARCHFILTER,
+            search_scope=SUBTREE,
+            attributes=['*'],
+            paged_size=1000,
+            generator=False
+        )
+        if len(conn.entries) == 1:
+            return conn.entries
+        return "group not exist"
+
+    def add_user_to_group(self, conn, username, group_name):
+        if conn == False:
+            return "bind failed"
+        search_user_res = self.search_user(conn, username)
+        if search_user_res == "user not exist":
+            return "user not exist"
+        search_group_res = self.search_group(conn, group_name)
+        if search_group_res == "group not exist":
+            return "group not exist"
+        user_dn = str(search_user_res['distinguishedName'])
+        group_dn = str(search_group_res[0].distinguishedName)
+        #print(user_dn)
+        #print(group_dn)
+        ad_add_members_to_groups(conn, user_dn, group_dn, fix=True)
+        return (conn.result)['description']
+
+    def remove_user_from_group(self, conn, username, group_name):
+        if conn == False:
+            return "bind failed"
+        search_user_res = self.search_user(conn, username)
+        if search_user_res == "user not exist":
+            return "user not exist"
+        search_group_res = self.search_group(conn, group_name)
+        if search_group_res == "group not exist":
+            return "group not exist"
+        user_dn = str(search_user_res['distinguishedName'])
+        group_dn = str(search_group_res[0].distinguishedName)
+        #print(user_dn)
+        #print(group_dn)
+        ad_remove_members_from_groups(conn, user_dn, group_dn, fix=True)
+        return (conn.result)['description']
